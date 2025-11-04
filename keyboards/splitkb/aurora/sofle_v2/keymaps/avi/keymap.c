c7753c509306e51089b4d0c322752d8e2703331d
Anton Vietrov <avietrov@users.noreply.github.com>
2025-11-03 20:27:26 -0500

    Use upper row for symbols.

--- keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
@@ -18,8 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     ),
     [_SYMBOLS] = LAYOUT(
-        XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PIPE,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        XXXXXXX, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+        XXXXXXX, KC_PLUS, KC_UNDS, KC_LCBR, KC_RCBR, KC_PIPE,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
         _______,  KC_EQL, KC_MINS, KC_LPRN, KC_RPRN, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                           _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
