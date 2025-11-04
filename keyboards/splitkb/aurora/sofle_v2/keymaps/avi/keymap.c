b62f6118b5152ad38e8b93df69d38231fa2820a5
Anton Vietrov <avietrov@users.noreply.github.com>
2025-11-03 20:38:00 -0500

    Swap modifiers around.

--- keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,   KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,                          KC_J,   KC_L,    KC_U,   KC_Y,KC_SCLN, KC_BSPC,
         KC_LCTL,   KC_A,   KC_R,   KC_S,   KC_T,   KC_G,                          KC_M,   KC_N,    KC_E,   KC_I,   KC_O, KC_QUOT,
         KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_D,   KC_V, XXXXXXX,    XXXXXXX,     KC_K,   KC_H, KC_COMM, KC_DOT,KC_SLSH,  KC_ESC,
-                        XXXXXXX,KC_HYPR,  MO(1),KC_LGUI,  KC_SPC,     KC_ENT,  KC_LALT,  MO(2), KC_MEH, XXXXXXX
+                        XXXXXXX,KC_HYPR,KC_LGUI,  MO(2),  KC_SPC,     KC_ENT,    MO(1),KC_LALT,  KC_MEH, XXXXXXX
 
     ),
     [_SYMBOLS] = LAYOUT(
