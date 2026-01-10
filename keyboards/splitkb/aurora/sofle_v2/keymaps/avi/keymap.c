87fd9e2616d0fb6ff0fad03593c392c844046752
Anton Vietrov <avietrov@users.noreply.github.com>
2026-01-10 11:27:49 -0500

    Rework navigation layers.

--- keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
@@ -8,10 +8,16 @@ enum layers {
     _NAVIGATION = 2,
 };
 
+// layer navigation
 #define MO_NAV MO(_NAVIGATION)
 #define MO_SYM MO(_SYMBOLS)
-#define GUI_SPC MT(MOD_LGUI, KC_SPC)
+
 #define OSL_SYM OSL(_SYMBOLS)
+
+#define TO_DEF TO(_DEFAULT)
+#define TO_NAV TO(_NAVIGATION)
+
+#define GUI_SPC MT(MOD_LGUI, KC_SPC)
 
 // home row mods
 #define HR_S MT(MOD_LALT, KC_S)
@@ -19,30 +25,31 @@ enum layers {
 #define HR_N MT(MOD_LGUI, KC_N)
 #define HR_E MT(MOD_LALT, KC_E)
 
+
 // would be good to be able to do cmd space and cmd enter
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT(
-         KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                          KC_6,   KC_7,    KC_8,   KC_9,   KC_0, XXXXXXX,
+         KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                          KC_6,   KC_7,    KC_8,   KC_9,   KC_0,  TO_DEF,
          KC_TAB,   KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,                          KC_J,   KC_L,    KC_U,   KC_Y,KC_SCLN, KC_BSPC,
         KC_LCTL,   KC_A,   KC_R,   HR_S,   HR_T,   KC_G,                          KC_M,   HR_N,    HR_E,   KC_I,   KC_O, KC_QUOT,
         KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_D,   KC_V, XXXXXXX,    XXXXXXX,     KC_K,   KC_H, KC_COMM, KC_DOT,KC_SLSH,  KC_ESC,
-                        XXXXXXX,XXXXXXX,XXXXXXX,OSL_SYM,  KC_SPC,     KC_ENT,   MO_NAV, XXXXXXX, XXXXXXX,OSL_SYM
+                        XXXXXXX,XXXXXXX,XXXXXXX,OSL_SYM,  KC_SPC,     KC_ENT,   TO_NAV, XXXXXXX, XXXXXXX,OSL_SYM
 
     ),
     [_SYMBOLS] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO_DEF,
         XXXXXXX, XXXXXXX,  KC_DLR,  KC_GRV, KC_LCBR, KC_RCBR,                       KC_AMPR, KC_HASH, KC_PLUS, KC_CIRC, KC_SCLN, _______,
         XXXXXXX, XXXXXXX, KC_EXLM, KC_UNDS, KC_LPRN, KC_RPRN,                         KC_AT, KC_MINS, KC_ASTR,  KC_EQL, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, KC_BSLS, KC_PIPE, KC_LBRC, KC_RBRC, _______,     _______, KC_TILD, KC_LABK, KC_RABK, KC_PERC, KC_SLSH, _______,
-                          _______, _______, _______, _______, _______,      KC_MEH, _______, _______, _______, _______
+                          _______, _______, _______, _______, _______,      KC_MEH, XXXXXXX, _______, _______, _______
 
     ),
     [_NAVIGATION] = LAYOUT(
-        XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
+        XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  TO_DEF,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
         KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, XXXXXXX, XXXXXXX,                       KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     _______, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
-                          _______, _______, _______, _______, KC_HYPR,     _______, _______, _______, _______, _______
+                          _______, _______, _______, _______, KC_HYPR,     _______, XXXXXXX, _______, _______, _______
 
     )
 };
