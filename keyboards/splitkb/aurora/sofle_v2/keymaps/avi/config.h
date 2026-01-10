7caed3869efbab274ac644de28b20973b21b9f96
Anton Vietrov <avietrov@users.noreply.github.com>
2026-01-10 11:13:24 -0500

    Setup home row mods.

--- keyboards/splitkb/aurora/sofle_v2/keymaps/avi/config.h
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/avi/config.h
@@ -1,1 +1,4 @@
 #pragma once
+
+#define TAPPING_TERM 300
+#define PERMISSIVE_HOLD
--- keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
@@ -13,14 +13,20 @@ enum layers {
 #define GUI_SPC MT(MOD_LGUI, KC_SPC)
 #define OSL_SYM OSL(_SYMBOLS)
 
+// home row mods
+#define HR_S MT(MOD_LALT, KC_S)
+#define HR_T MT(MOD_LGUI, KC_T)
+#define HR_N MT(MOD_LGUI, KC_N)
+#define HR_E MT(MOD_LALT, KC_E)
+
 // would be good to be able to do cmd space and cmd enter
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT(
          KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                          KC_6,   KC_7,    KC_8,   KC_9,   KC_0, XXXXXXX,
          KC_TAB,   KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,                          KC_J,   KC_L,    KC_U,   KC_Y,KC_SCLN, KC_BSPC,
-        KC_LCTL,   KC_A,   KC_R,   KC_S,   KC_T,   KC_G,                          KC_M,   KC_N,    KC_E,   KC_I,   KC_O, KC_QUOT,
+        KC_LCTL,   KC_A,   KC_R,   HR_S,   HR_T,   KC_G,                          KC_M,   HR_N,    HR_E,   KC_I,   KC_O, KC_QUOT,
         KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_D,   KC_V, XXXXXXX,    XXXXXXX,     KC_K,   KC_H, KC_COMM, KC_DOT,KC_SLSH,  KC_ESC,
-                        KC_LGUI,XXXXXXX,KC_LALT,OSL_SYM,  KC_SPC,     KC_ENT,   MO_NAV, XXXXXXX, XXXXXXX,OSL_SYM
+                        XXXXXXX,XXXXXXX,XXXXXXX,OSL_SYM,  KC_SPC,     KC_ENT,   MO_NAV, XXXXXXX, XXXXXXX,OSL_SYM
 
     ),
     [_SYMBOLS] = LAYOUT(
