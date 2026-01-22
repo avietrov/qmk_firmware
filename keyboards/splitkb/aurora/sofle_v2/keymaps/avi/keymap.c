adb930a10eb313e0614ee852fd7607cbed7b3168
Anton Vietrov <avietrov@users.noreply.github.com>
2026-01-21 21:52:35 -0500

    Tweak nav, fix meh and hypr.

--- keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/avi/keymap.c
@@ -38,11 +38,11 @@ enum layers {
 // would be good to be able to do cmd space and cmd enter
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT(
-         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  TO_DEF,
+         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
          KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                          KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
         KC_LCTL,    KC_A,    KC_R,    HR_S,    HR_T,    KC_G,                          KC_M,    HR_N,    HR_E,    KC_I,    KC_O, KC_QUOT,
         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, XXXXXXX,     XXXXXXX,    KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
-                          MOD_MEH, XXXXXXX, XXXXXXX, OSL_SYM,  KC_SPC,      KC_ENT,  TO_NAV, XXXXXXX, XXXXXXX,MOD_HYPR
+                           KC_MEH, XXXXXXX, XXXXXXX, OSL_SYM,  KC_SPC,      KC_ENT,  MO_NAV, XXXXXXX, XXXXXXX, KC_HYPR
 
     ),
     [_SYMBOLS] = LAYOUT(
@@ -55,10 +55,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NAVIGATION] = LAYOUT(
          TO_ADJ, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO_DEF,
-        XXXXXXX, MS_BTN1,   MS_UP, MS_BTN2, MS_WHLU, XXXXXXX,                       NV_BACK, NV_PREV, NV_NEXT, NV_FRWD, XXXXXXX, KC_BSPC,
-        KC_LCTL, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLD, XXXXXXX,                       KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
-        XXXXXXX, MS_WHLL, XXXXXXX, MS_WHLR, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ESC,
-                          XXXXXXX, XXXXXXX, KC_LALT, KC_LSFT, KC_LGUI,     MS_ACL0, MS_ACL1, MS_ACL2, XXXXXXX, XXXXXXX
+         KC_TAB, XXXXXXX, MS_BTN1,   MS_UP, MS_BTN2, MS_WHLD,                       NV_BACK, NV_PREV, NV_NEXT, NV_FRWD, XXXXXXX, KC_BSPC,
+        KC_LCTL, XXXXXXX, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLU,                       KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX,  TO_NAV,
+        KC_LSFT, XXXXXXX, MS_WHLL, XXXXXXX, MS_WHLR, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_ESC,
+                          KC_LCTL, KC_LALT, KC_LGUI,  TO_DEF,  KC_SPC,      KC_ENT, XXXXXXX, MS_ACL0, MS_ACL1, MS_ACL2
 
     ),
     [_ADJUST] = LAYOUT(
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                          KC_7,    KC_8,    KC_9, KC_MINS, XXXXXXX,  KC_DEL,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          KC_4,    KC_5,    KC_6, KC_PLUS, XXXXXXX,  KC_DEL,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,    KC_1,    KC_2,    KC_3,  KC_ENT, XXXXXXX, XXXXXXX,
-                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,    KC_0,    KC_0,  KC_DOT,  KC_ENT
+                          XXXXXXX, XXXXXXX, XXXXXXX,  TO_DEF, XXXXXXX,     XXXXXXX,    KC_0,    KC_0,  KC_DOT,  KC_ENT
 
     ),
 };
